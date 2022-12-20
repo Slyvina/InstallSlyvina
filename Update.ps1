a79bf5c4ce54fc7349183f80e7fce8cae5ad58ee
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-20 10:45:15 +0100

    cleaner syntax

--- Update.ps1
+++ Update.ps1
@@ -1,5 +1,5 @@
-echo 'Library Headers'; cd Libs/Headers; git poll;cd ../..
-echo 'Library JCR6'; cd Libs/JCR6; git poll;cd ../..
-echo 'Library Kthura'; cd Libs/Kthura; git poll;cd ../..
-echo 'Library TQSL'; cd Libs/TQSL; git poll;cd ../..
-echo 'Library Units'; cd Libs/Units; git poll;cd ../..
+echo 'Library Headers'; cd "Libs/Headers"; git poll;cd ../..
+echo 'Library JCR6'; cd "Libs/JCR6"; git poll;cd ../..
+echo 'Library Kthura'; cd "Libs/Kthura"; git poll;cd ../..
+echo 'Library TQSL'; cd "Libs/TQSL"; git poll;cd ../..
+echo 'Library Units'; cd "Libs/Units"; git poll;cd ../..
