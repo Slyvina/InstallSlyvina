b952cc4c8c1798e1e65e0db26df37f4521885edc
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-20 10:41:52 +0100

    Scripts v1

---
+++ Install.ps1
@@ -0,0 +1,9 @@
+md Apps
+md Tools
+
+
+echo 'Library Headers'; git clone https ://github.com/Slyvina/Headers.git Libs/Headers
+echo 'Library JCR6'; git clone https ://github.com/Slyvina/JCR6.git Libs/JCR6
+echo 'Library Kthura'; git clone https ://github.com/Slyvina/Kthura.git Libs/Kthura
+echo 'Library TQSL'; git clone https ://github.com/Slyvina/TQSL.git Libs/TQSL
+echo 'Library Units'; git clone https ://github.com/Slyvina/Units.git Libs/Units
